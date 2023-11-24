2359e1ef23e6c53bae2c073ada8540ee5a80e4ae
ChisomNwosu <150328063+ChisomNwosu@users.noreply.github.com>
2023-11-24 22:03:49 +0100

    BMI_calculator.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -1,19 +1,19 @@
 name = input("Enter your name: ")
 weight = int(input("Enter your weight in pounds: "))
 height= int(input("Enter your height in inches: "))
-Bmi = (weight * 703) / (height * height)
-print(Bmi)
+BMI = (weight * 703) / (height * height)
+print(BMI)
 
-if Bmi > 0:
-    if (Bmi <= 18.5):
+if BMI > 0:
+    if (BMI <= 18.5):
         print(name +", you are under weight")
-    elif (Bmi <= 24.9):
+    elif (BMI <= 24.9):
         print(name + ", you are normal weight")
-    elif (Bmi <= 29.9):
+    elif (BMI <= 29.9):
         print(name + ", you are overweight, you need to exercise more and stop playing games")
-    elif (Bmi <= 34.9):
+    elif (BMI <= 34.9):
         print(name + ", you are obese")
-    elif (Bmi <= 39.9):
+    elif (BMI <= 39.9):
         print(name +", you are severly obese ")
     else:
         print(name + ", you are mobidly obese")      
